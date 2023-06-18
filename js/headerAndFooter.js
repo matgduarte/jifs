013075cf41b2db822183a5076fad2819770312e5
matgduarte <matheusginezduarte@gmail.com>
2023-06-18 05:52:15 -0300

    cronograma e boletim com interaçao js

--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -1,3 +1,4 @@
+/*header e imagens do index*/
 window.addEventListener('scroll', function () {
     var div = document.querySelector('header');
     var div2 = document.querySelector('.btn');
@@ -5,7 +6,7 @@ window.addEventListener('scroll', function () {
     var div4 = document.querySelector('#desenho2');
     var scrollPosition = window.scrollY;
 
-    if (scrollPosition > 100) {
+    if (scrollPosition > 0) {
         div.classList.add('scrolled');
         div2.classList.add('scrolled');
         div3.classList.add('scrolled');
@@ -17,6 +18,8 @@ window.addEventListener('scroll', function () {
         div4.classList.remove('scrolled');
     }
 });
+
+/*footer*/
 var b = document.getElementsByClassName('b-equipe')[0];
 var div5 = document.getElementsByClassName('equipe')[0];
 var div6 = document.getElementsByClassName('link')[0];
@@ -43,6 +46,7 @@ b.addEventListener('click', function () {
         isClicado = false;
     }
 });
+
 /*Menu Responsivo*/
 let menu = document.querySelector("nav");
 document.querySelector("#menu-btn").onclick = () => {
@@ -50,4 +54,47 @@ document.querySelector("#menu-btn").onclick = () => {
 }
 window.onscroll = () => {
     menu.classList.remove("ativo");
+}
+
+/*altera cronograma*/
+function alterarConteudo() {
+    var selectElement = document.getElementById("menu-dia");
+    var paragraphElement = document.getElementById("p-cron");
+    var selectedOption = selectElement.value;
+
+    if (selectedOption === "dia1") {
+        paragraphElement.innerHTML = "Conteúdo para a opção 1.";
+    } else if (selectedOption === "dia2") {
+        paragraphElement.innerHTML = "Conteúdo para a opção 2.";
+    } else if (selectedOption === "dia3") {
+        paragraphElement.innerHTML = "Conteúdo para a opção 3.";
+    }
+}
+
+function abrirPaginaFlutuante1() {
+    var paginaFlutuante1 = document.getElementById("paginaFlutuante1");
+    paginaFlutuante1.style.display = "flex";
+}
+function abrirPaginaFlutuante2() {
+    var paginaFlutuante2 = document.getElementById("paginaFlutuante2");
+    paginaFlutuante2.style.display = "flex";
+}
+function abrirPaginaFlutuante3() {
+    var paginaFlutuante3 = document.getElementById("paginaFlutuante3");
+    paginaFlutuante3.style.display = "flex";
+}
+function abrirPaginaFlutuante4() {
+    var paginaFlutuante4 = document.getElementById("paginaFlutuante4");
+    paginaFlutuante4.style.display = "flex";
+}
+
+function fecharPaginaFlutuante() {
+    var paginaFlutuante1 = document.getElementById("paginaFlutuante1");
+    paginaFlutuante1.style.display = "none";
+    var paginaFlutuante2 = document.getElementById("paginaFlutuante2");
+    paginaFlutuante2.style.display = "none";
+    var paginaFlutuante3 = document.getElementById("paginaFlutuante3");
+    paginaFlutuante3.style.display = "none";
+    var paginaFlutuante4 = document.getElementById("paginaFlutuante4");
+    paginaFlutuante4.style.display = "none";
 }
