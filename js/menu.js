a6a5659137168b63948b1e2f428764cc04e4189a
matgduarte <matheusginezduarte@gmail.com>
2023-06-25 03:19:46 -0300

    teste do menu com js
    nao esta pronto, fui dormir to com sono :(

---
+++ js/menu.js
@@ -0,0 +1,47 @@
+class MobileNavbar {
+    constructor(mobileMenu, pag, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.pag = document.querySelector(pag);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.pag.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".pag",
+    ".pag li",
+  );
+  mobileNavbar.init();
+  
+  
+  
