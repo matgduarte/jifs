dc8492df5d5dc4a87af3577ff7f9eacd682957dd
matgduarte <matheusginezduarte@gmail.com>
2023-06-28 01:15:49 -0300

    menu, index, js compacto, problemas

--- js/headerAndFooter.js
+++ js/headerAndFooter.js
@@ -1,5 +1,5 @@
-/*header e imagens do index*/
-window.addEventListener('scroll', function () {
+/*funçao scrolled e menu*/
+window.addEventListener('load', function () {
     var div = document.querySelector('header');
     var div2 = document.querySelector('.btn');
     var div3 = document.querySelector('#desenho1');
@@ -11,13 +11,82 @@ window.addEventListener('scroll', function () {
         div2.classList.add('scrolled');
         div3.classList.add('scrolled');
         div4.classList.add('scrolled');
-    } else {
-        div.classList.remove('scrolled');
-        div2.classList.remove('scrolled');
-        div3.classList.remove('scrolled');
-        div4.classList.remove('scrolled');
     }
+
+    window.addEventListener('scroll', function () {
+        if (!document.querySelector('.mobile-menu.active')) {
+            var scrollPosition = window.scrollY;
+
+            if (scrollPosition > 0) {
+                div.classList.add('scrolled');
+                div2.classList.add('scrolled');
+                div3.classList.add('scrolled');
+                div4.classList.add('scrolled');
+            } else {
+                div.classList.remove('scrolled');
+                div2.classList.remove('scrolled');
+                div3.classList.remove('scrolled');
+                div4.classList.remove('scrolled');
+            }
+        }
+    });
 });
+
+class MobileNavbar {
+    constructor(mobileMenu, pag, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.pag = document.querySelector(pag);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+      
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.3
+            }s`);
+        });
+    }
+    
+    handleClick() {
+        var div = document.querySelector('header');
+        var div2 = document.querySelector('.btn');
+        var div3 = document.querySelector('#desenho1');
+        var div4 = document.querySelector('#desenho2');
+        
+        div.classList.toggle('scrolled');
+        div2.classList.toggle('scrolled');
+        div3.classList.toggle('scrolled');
+        div4.classList.toggle('scrolled');
+        
+        this.pag.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+    
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+    
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".pag",
+    ".pag li",
+);
+mobileNavbar.init();
+
 
 /*footer*/
 var b = document.getElementsByClassName('b-equipe')[0];
@@ -47,30 +116,6 @@ b.addEventListener('click', function () {
     }
 });
 
-/*Menu Responsivo*/
-let menu = document.querySelector("nav");
-document.querySelector("#menu-btn").onclick = () => {
-    menu.classList.toggle("ativo");
-}
-window.onscroll = () => {
-    menu.classList.remove("ativo");
-}
-
-/*altera cronograma*/
-function alterarConteudo() {
-    var selectElement = document.getElementById("menu-dia");
-    var paragraphElement = document.getElementById("p-cron");
-    var selectedOption = selectElement.value;
-
-    if (selectedOption === "dia1") {
-        paragraphElement.innerHTML = "Conteúdo para a opção 1.";
-    } else if (selectedOption === "dia2") {
-        paragraphElement.innerHTML = "Conteúdo para a opção 2.";
-    } else if (selectedOption === "dia3") {
-        paragraphElement.innerHTML = "Conteúdo para a opção 3.";
-    }
-}
-
 function abrirPaginaFlutuante1() {
     var paginaFlutuante1 = document.getElementById("paginaFlutuante1");
     paginaFlutuante1.style.display = "flex";
@@ -98,3 +143,10 @@ function fecharPaginaFlutuante() {
     var paginaFlutuante4 = document.getElementById("paginaFlutuante4");
     paginaFlutuante4.style.display = "none";
 }
+
+
+/*altura header*/
+
+let header = document.querySelector("#header");
+let headerHeight = getComputedStyle(header).height;
+document.documentElement.style.setProperty("--header-height", headerHeight);
